bf6cb61add2733ecd133a98714e719b75415f9d0
Jakub Pastuszek <jpastuszek@gmail.com>
2016-07-29 15:26:40 +0100

    moved VSL parsers to vsl.rs

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,7 @@ extern crate stderrlog;
 extern crate clap;
 use clap::{Arg, App};
 
-use std::fmt::{self, Debug};
 use std::io::{self, stdin};
-use std::str::Utf8Error;
-use nom::{le_u32};
-use std::mem;
 
 #[macro_use]
 mod stream_buf;
@@ -22,121 +18,9 @@ use stream_buf::{StreamBuf, ReadStreamBuf, FillError, FillApplyError};
 // https://github.com/varnishcache/varnish-cache/blob/master/include/tbl/vsl_tags.h
 // https://github.com/varnishcache/varnish-cache/blob/master/include/tbl/vsl_tags_http.h
 mod vsl_tag_e;
-use vsl_tag_e::VSL_tag_e as VslRecordTag;
-
-/*
- * Shared memory log format
- *
- * The log member points to an array of 32bit unsigned integers containing
- * log records.
- *
- * Each logrecord consist of:
- *    [n]               = ((type & 0xff) << 24) | (length & 0xffff)
- *    [n + 1]           = ((marker & 0x03) << 30) | (identifier & 0x3fffffff)
- *    [n + 2] ... [m]   = content (NUL-terminated)
- *
- * Logrecords are NUL-terminated so that string functions can be run
- * directly on the shmlog data.
- *
- * Notice that the constants in these macros cannot be changed without
- * changing corresponding magic numbers in varnishd/cache/cache_shmlog.c
- */
-
-const VSL_LENOFFSET: u32 = 24;
-const VSL_LENMASK: u32 = 0xffff;
-const VSL_MARKERMASK: u32 = 0x03;
-//const VSL_CLIENTMARKER: u32 = 1 << 30;
-//const VSL_BACKENDMARKER: u32 = 1 << 31;
-const VSL_IDENTOFFSET: u32 = 30;
-const VSL_IDENTMASK: u32 = !(3 << VSL_IDENTOFFSET);
-
-named!(binary_vsl_tag<&[u8], &[u8]>, tag!(b"VSL\0"));
-
-#[derive(Debug)]
-struct VslRecordHeader {
-    pub tag: u8,
-    pub len: u16,
-    pub marker: u8,
-    pub ident: u32,
-}
-
-fn vsl_record_header<'b>(input: &'b[u8]) -> nom::IResult<&'b[u8], VslRecordHeader, u32> {
-    chain!(
-        input, r1: le_u32 ~ r2: le_u32,
-        || {
-            VslRecordHeader {
-                tag: (r1 >> VSL_LENOFFSET) as u8,
-                len: (r1 & VSL_LENMASK) as u16,
-                marker: (r2 & VSL_MARKERMASK >> VSL_IDENTOFFSET) as u8,
-                ident: r2 & VSL_IDENTMASK,
-            }
-        })
-}
-
-struct VslRecord<'b> {
-    pub tag: VslRecordTag,
-    pub marker: u8,
-    pub ident: u32,
-    pub data: &'b[u8],
-}
-
-impl<'b> VslRecord<'b> {
-    fn body(&'b self) -> Result<&'b str, Utf8Error> {
-        std::str::from_utf8(self.data)
-    }
-}
-
-impl<'b> Debug for VslRecord<'b> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        f.debug_struct("VSL Record")
-            .field("tag", &self.tag)
-            .field("marker", &self.marker)
-            .field("ident", &self.ident)
-            .field("body", &self.body())
-            .finish()
-    }
-}
-
-fn to_vsl_record_tag(num: u8) -> VslRecordTag {
-    // Tend to work even for missing tags as they end up as SLT__Bogus (0)
-    unsafe { mem::transmute(num as u32) }
-}
-
-fn vsl_record_v3<'b>(input: &'b[u8]) -> nom::IResult<&'b[u8], VslRecord<'b>, u32> {
-    chain!(
-        input,
-        header: vsl_record_header ~ data: take!(header.len) ~ take!((4 - header.len % 4) % 4),
-        || {
-            VslRecord {
-                tag: to_vsl_record_tag(header.tag),
-                marker: header.marker,
-                ident: header.ident,
-                data: data
-            }
-        })
-}
-
-fn vsl_record_v4<'b>(input: &'b[u8]) -> nom::IResult<&'b[u8], VslRecord<'b>, u32> {
-    chain!(
-        input,
-        header: vsl_record_header ~ data: take!(header.len - 1) ~ take!(1) ~ take!((4 - header.len % 4) % 4),
-        || {
-            VslRecord {
-                tag: to_vsl_record_tag(header.tag),
-                marker: header.marker,
-                ident: header.ident,
-                data: data
-            }
-        })
-}
-
-/*
-fn binary_vsl_records<'b>(input: &'b[u8]) -> nom::IResult<&'b[u8], Vec<VslRecord<'b>>, u32> {
-    chain!(
-        input, binary_vsl_tag ~ records: many1!(vsl_record),
-        || { records })
-}
-*/
+mod vsl;
+use vsl::{binary_vsl_tag, vsl_record_v3, vsl_record_v4};
+use vsl::{VslRecord, VslRecordTag};
 
 fn main() {
     let arguments = App::new("Varnish VSL log to syslog logger")
