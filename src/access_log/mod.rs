08f0361c14ce2c959b934aabe8ddb90015cf96f3
Jakub Pastuszek <jpastuszek@gmail.com>
2016-08-24 10:29:38 +0100

    refactoring

--- src/access_log/mod.rs
+++ src/access_log/mod.rs
@@ -90,27 +90,6 @@ pub enum Format {
     JsonPretty,
 }
 
-trait TransactionLookup {
-    fn find_full<'a>(&'a self, restart_count: usize) -> Option<(&'a Self, usize)>;
-}
-
-impl TransactionLookup for ClientAccessRecord {
-    fn find_full<'a>(&'a self, restart_count: usize) -> Option<(&'a Self, usize)> {
-        match self.transaction {
-            ClientAccessTransaction::Full { .. } => Some((self, restart_count)),
-            ClientAccessTransaction::Restarted { ref restart_record, .. } => {
-                if let Some(record) = restart_record.get_resolved() {
-                    record.find_full(restart_count + 1)
-                } else {
-                    warn!("Found unresolved link {:?} in: {:?}", restart_record, self);
-                    None
-                }
-            },
-            ClientAccessTransaction::Piped { .. } => None,
-        }
-    }
-}
-
 trait AsSer<'a> {
     type Out;
 
@@ -208,31 +187,49 @@ impl AccessLog for SessionRecord {
             Ok(())
         }
 
-        fn log_client_record<W>(
-            format: &Format, out: &mut W,
+        fn find_final(record: &ClientAccessRecord, restart_count: usize) -> Option<(&ClientAccessRecord, usize)> {
+            match record.transaction {
+                ClientAccessTransaction::Full { .. } | ClientAccessTransaction::Piped { .. } => Some((record, restart_count)),
+                ClientAccessTransaction::Restarted { ref restart_record, .. } => {
+                    if let Some(record) = restart_record.get_resolved() {
+                        find_final(record, restart_count + 1)
+                    } else {
+                        warn!("Found unresolved link {:?} in: {:?}", restart_record, record);
+                        None
+                    }
+                },
+            }
+        }
+
+        fn log_linked_client_access_record<W>(
+            format: &Format,
+            out: &mut W,
             session_record: &SessionRecord,
             record_link: &Link<ClientAccessRecord>,
             request_type: &'static str) -> Result<(), OutputError> where W: Write {
             if let Some(record) = record_link.get_resolved() {
-                match record.transaction {
-                    ClientAccessTransaction::Full {
-                        ref esi_records,
-                        ref request,
-                        ref response,
-                        process,
-                        fetch,
-                        ttfb,
-                        serve,
-                        ref accounting,
-                        ..
-                    } => {
-                        try!(write(format, out, &ClientAccessLogEntry {
+                if let Some((final_record, restart_count)) = find_final(record, 0) {
+                    // Note: we skip all the intermediate restart records
+                    match (&record.transaction, &final_record.transaction) {
+                        (&ClientAccessTransaction::Restarted {
+                            ref request,
+                            process,
+                            ..
+                        }, &ClientAccessTransaction::Full {
+                            ref esi_records,
+                            ref response,
+                            fetch,
+                            ttfb,
+                            serve,
+                            ref accounting,
+                            ..
+                        }) => try!(write(format, out, &ClientAccessLogEntry {
                             request_type: request_type,
                             remote_address: session_record.remote.as_ser(),
                             session_timestamp: session_record.open,
                             start_timestamp: record.start,
-                            end_timestamp: record.end,
-                            handing: record.handling.as_ser(),
+                            end_timestamp: final_record.end,
+                            handing: final_record.handling.as_ser(),
                             request: request.as_ser(),
                             response: response.as_ser(),
                             process: process,
@@ -246,100 +243,92 @@ impl AccessLog for SessionRecord {
                             sent_body_bytes: accounting.sent_body,
                             sent_total_bytes: accounting.sent_total,
                             esi_count: esi_records.len(),
-                            restart_count: 0,
+                            restart_count: restart_count,
+                            restart_log: Some(record.log.as_ser()),
+                            log: final_record.log.as_ser(),
+                        })),
+                        (_, &ClientAccessTransaction::Full {
+                            ref esi_records,
+                            ref request,
+                            ref response,
+                            process,
+                            fetch,
+                            ttfb,
+                            serve,
+                            ref accounting,
+                            ..
+                        }) => try!(write(format, out, &ClientAccessLogEntry {
+                            request_type: request_type,
+                            remote_address: session_record.remote.as_ser(),
+                            session_timestamp: session_record.open,
+                            start_timestamp: final_record.start,
+                            end_timestamp: final_record.end,
+                            handing: final_record.handling.as_ser(),
+                            request: request.as_ser(),
+                            response: response.as_ser(),
+                            process: process,
+                            fetch: fetch,
+                            ttfb: ttfb,
+                            serve: serve,
+                            recv_header_bytes: accounting.recv_header,
+                            recv_body_bytes: accounting.recv_body,
+                            recv_total_bytes: accounting.recv_total,
+                            sent_header_bytes: accounting.sent_header,
+                            sent_body_bytes: accounting.sent_body,
+                            sent_total_bytes: accounting.sent_total,
+                            esi_count: esi_records.len(),
+                            restart_count: restart_count,
                             restart_log: None,
-                            log: record.log.as_ser(),
-                        }));
-
-                        for esi_record_link in esi_records {
-                            try!(log_client_record(format, out, session_record, esi_record_link, "esi"))
-                        }
-                    }
-                    ClientAccessTransaction::Restarted {
-                        ref request,
-                        process,
-                        ..
-                    } => {
-                        if let Some((full_record, restart_count)) = record.find_full(0) {
-                            if let ClientAccessTransaction::Full {
-                                ref esi_records,
-                                ref response,
-                                fetch,
-                                ttfb,
-                                serve,
-                                ref accounting,
-                                ..
-                            } = full_record.transaction {
-                                try!(write(format, out, &ClientAccessLogEntry {
-                                    request_type: request_type,
-                                    remote_address: session_record.remote.as_ser(),
-                                    session_timestamp: session_record.open,
-                                    start_timestamp: record.start,
-                                    end_timestamp: full_record.end,
-                                    handing: full_record.handling.as_ser(),
-                                    request: request.as_ser(),
-                                    response: response.as_ser(),
-                                    process: process,
-                                    fetch: fetch,
-                                    ttfb: ttfb,
-                                    serve: serve,
-                                    recv_header_bytes: accounting.recv_header,
-                                    recv_body_bytes: accounting.recv_body,
-                                    recv_total_bytes: accounting.recv_total,
-                                    sent_header_bytes: accounting.sent_header,
-                                    sent_body_bytes: accounting.sent_body,
-                                    sent_total_bytes: accounting.sent_total,
-                                    esi_count: esi_records.len(),
-                                    restart_count: restart_count,
-                                    restart_log: Some(record.log.as_ser()),
-                                    //TODO: still will miss intermediate log entreis
-                                    log: full_record.log.as_ser(),
-                                }));
-
-                                for esi_record_link in esi_records {
-                                    try!(log_client_record(format, out, session_record, esi_record_link, "esi"))
+                            log: final_record.log.as_ser(),
+                        })),
+                        (_, &ClientAccessTransaction::Piped {
+                            ref request,
+                            ref backend_record,
+                            process,
+                            ttfb,
+                            ..
+                        }) => {
+                            if let Some(backend_record) = backend_record.get_resolved() {
+                                if let BackendAccessTransaction::Piped {
+                                    request: ref backend_request,
+                                    ref backend_connection,
+                                    ..
+                                } = backend_record.transaction {
+                                    try!(write(format, out, &PipeSessionLogEntry {
+                                        remote_address: session_record.remote.as_ser(),
+                                        session_timestamp: session_record.open,
+                                        start_timestamp: final_record.start,
+                                        end_timestamp: final_record.end,
+                                        handing: final_record.handling.as_ser(),
+                                        request: request.as_ser(),
+                                        backend_request: backend_request.as_ser(),
+                                        process: process,
+                                        ttfb: ttfb,
+                                        log: final_record.log.as_ser(),
+                                        backend_connection: backend_connection.as_ser(),
+                                    }))
+                                } else {
+                                    warn!("Expected Piped ClientAccessRecord to link Piped BackendAccessTransaction; link {:?} in: {:?}",
+                                          backend_record, final_record);
                                 }
                             } else {
-                                panic!("ClientAccessRecord::find_full found not ClientAccessTransaction::Full: {:?}", record)
+                                warn!("Found unresolved link {:?} in: {:?}", backend_record, final_record);
                             }
-                        } else {
-                            //TODO: what about piped? etc? need to find_leaf record and handle it somehow
-                            warn!("Full transaction not found on restarted transaction chain: {:?}", record);
                         }
-                    },
-                    ClientAccessTransaction::Piped {
-                        ref request,
-                        ref backend_record,
-                        process,
-                        ttfb,
-                        ..
-                    } => {
-                        if let Some(backend_record) = backend_record.get_resolved() {
-                            if let BackendAccessTransaction::Piped {
-                                request: ref backend_request,
-                                ref backend_connection,
-                                ..
-                            } = backend_record.transaction {
-                                try!(write(format, out, &PipeSessionLogEntry {
-                                    remote_address: session_record.remote.as_ser(),
-                                    session_timestamp: session_record.open,
-                                    start_timestamp: record.start,
-                                    end_timestamp: record.end,
-                                    handing: record.handling.as_ser(),
-                                    request: request.as_ser(),
-                                    backend_request: backend_request.as_ser(),
-                                    process: process,
-                                    ttfb: ttfb,
-                                    log: record.log.as_ser(),
-                                    backend_connection: backend_connection.as_ser(),
-                                }))
-                            } else {
-                                warn!("Piped ClientAccessRecord has not Piped BackendAccessTransaction linked: {:?} linked: {:?}", record, backend_record);
-                            }
-                        } else {
-                            warn!("Found unresolved link {:?} in: {:?}", backend_record, record);
-                        }
-                    },
+                        (_, &ClientAccessTransaction::Restarted { .. }) => panic!("got ClientAccessTransaction::Restarted as final final_record")
+                    }
+
+                    match &final_record.transaction {
+                        &ClientAccessTransaction::Full {
+                            ref esi_records,
+                            ..
+                        } => for esi_record_link in esi_records {
+                            try!(log_linked_client_access_record(format, out, session_record, esi_record_link, "ESI"))
+                        },
+                        &ClientAccessTransaction::Restarted { .. } | &ClientAccessTransaction::Piped { .. } => (),
+                    }
+                } else {
+                    warn!("Failed to find final record for: {:?}", record);
                 }
             } else {
                 warn!("Found unresolved link {:?} in: {:?}", record_link, session_record);
@@ -348,7 +337,7 @@ impl AccessLog for SessionRecord {
         }
 
         for record_link in self.client_records.iter() {
-            try!(log_client_record(format, out, self, record_link, "remote"))
+            try!(log_linked_client_access_record(format, out, self, record_link, "external"))
         }
         Ok(())
     }
