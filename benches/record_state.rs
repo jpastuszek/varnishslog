fd24603b72b83b7854a9d4bd572e3b983d3768f2
Jakub Pastuszek <jpastuszek@gmail.com>
2016-09-20 16:45:09 +0100

    SessionState bench

--- benches/record_state.rs
+++ benches/record_state.rs
@@ -11,6 +11,7 @@ extern crate varnishslog;
 use varnishslog::stream_buf::{ReadStreamBuf, StreamBuf, FillApplyError, FillError};
 use varnishslog::access_log::{binary_vsl_tag, vsl_record_v4, VslRecord};
 use varnishslog::access_log::RecordState;
+use varnishslog::access_log::SessionState;
 
 fn parse_each_vsl_record<R: Read, C>(mut rfb: ReadStreamBuf<R>, mut block: C) where C: FnMut(&VslRecord) {
     rfb.fill_apply(binary_vsl_tag).unwrap();
@@ -39,6 +40,7 @@ fn parse_each_vsl_record<R: Read, C>(mut rfb: ReadStreamBuf<R>, mut block: C) wh
 }
 
 fn open_data() -> File {
+    // 13480 VSL records making up 211 access records and 86 sessions
     File::open("benches/varnish20160816-4093-1xh1jbx808a493d5e74216e5.vsl").unwrap()
 }
 
@@ -49,7 +51,6 @@ fn load_data() -> Vec<u8> {
 }
 
 fn access_record_parsing(bench: &mut Bencher) {
-    // 13480 VSL records making up 211 access records
     let mut cursor = Cursor::new(load_data());
     bench.iter(|| {
         let mut rs = RecordState::new();
@@ -67,5 +68,26 @@ fn access_record_parsing(bench: &mut Bencher) {
     });
 }
 
-benchmark_group!(benches, access_record_parsing);
+fn access_session_parsing(bench: &mut Bencher) {
+    let mut cursor = Cursor::new(load_data());
+    bench.iter(|| {
+        let mut ss = SessionState::new();
+        // use larger data sample
+        for _ in 0..4 {
+            {
+                parse_each_vsl_record(ReadStreamBuf::new(&mut cursor), |vsl_record| {
+                    bencher::black_box(ss.apply(vsl_record));
+                });
+            }
+            cursor.set_position(0);
+        }
+        assert_eq!(ss.unmatched_client_access_records().len(), 0);
+        assert_eq!(ss.unmatched_backend_access_records().len(), 0);
+        assert_eq!(ss.unresolved_sessions().len(), 0);
+    });
+}
+
+benchmark_group!(benches,
+                 access_record_parsing,
+                 access_session_parsing);
 benchmark_main!(benches);
